66bf5223a77be303549bc82ca8f66554e34f781d
+ <+>
2022-04-06 10:57:09 -0400

    commit tree 7cdfea5f489878c19e21729409fc9044bc80ca04

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
-// code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+  ]
+
+
+function superbowlWin(array){
+    if(array.find(checkWin)){
+        return array.find(checkWin).year
+    }
+}
+function checkWin(record){
+    return record.result === 'W'
+}
+
+superbowlWin(checkWin);
+
+// function superbowlWin(object) {
+//     let result = object.find(checkWin)
+//     debugger
+//     if(result){
+//         return result.year
+//     }else {
+//         return undefined
+//     }
+// }
+
+
+// let checkWin = record.find(records => records.result ==='W');
+
+function checkWin(record){
+    return record.result === 'W'
+}
+
+superbowlWin(checkWin);
+
+
+// record.find(superbowlWin);
+
+//   function superbowlWin(record){
+//       let winYear = undefined;
+//       for(let item of record) {
+//           if (item.result === 'W' && item.year === '2015'){
+//           winYear = item.year
+//           return winYear;
+//           }
+//       }
+// //   }
